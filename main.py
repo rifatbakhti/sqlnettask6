7f6d2aa44b45f5639a15312cf6ca8ea22da94479
Рифат Бахтиев <rifatbakhtiev@gmail.com>
2023-03-01 18:35:57 +0400

    Second commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sqlalchemy #mb it won`t work
+from datetime import datetime
 from sqlalchemy.orm import sessionmaker
 
-from models import create_tables, Publisher, Book
+from models import create_tables, Publisher, Book, Shop, Stock, Sale
 
-DSN = 'postgresql://postgres:123@localhost:5432/sqlnettask5' #мб не сработает база данных
+DSN = 'postgresql://postgres:123@localhost:5432/sqlnettask6' #мб не сработает база данных. Вроде пока работает
 engine = sqlalchemy.create_engine(DSN)
 
 create_tables(engine)
@@ -11,5 +12,62 @@ create_tables(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+publisher1 = Publisher(name='Пушкин')
+session.add(publisher1)
+session.commit()
+
+book1 = Book(title='Капитанская дочка', id_publisher=1)
+session.add(book1)
+book2 = Book(title='Руслан и Людмила', id_publisher=1)
+session.add(book2)
+book3 = Book(title='Евгений Онегин', id_publisher=1)
+session.add(book3)
+session.commit()
+print(book3.title)
+
+shop1 = Shop(name='Буквоед')
+session.add(shop1)
+shop2 = Shop(name='Лабиринт')
+session.add(shop2)
+shop3 = Shop(name='Книжный дом')
+session.add(shop3)
+session.commit()
+print(shop3.name)
+
+stock1 = Stock(id_book=1, id_shop=1)
+session.add(stock1)
+stock2 = Stock(id_book=2, id_shop=1)
+session.add(stock2)
+stock3 = Stock(id_book=1, id_shop=3)
+session.add(stock3)
+stock4 = Stock(id_book=3, id_shop=3)
+session.add(stock4)
+stock5 = Stock(id_book=1, id_shop=1)
+session.add(stock5) #такое ощущение, что он не создает новые связи, а перезаписывает старые
+session.commit()
+# print(stock1.id_book)
+
+sale1 = Sale(prise=600, date_sale=datetime(2022, 11, 9), id_stock=1)
+session.add(sale1)
+sale2 = Sale(prise=500, date_sale=datetime(2022, 11, 8), id_stock=2)
+session.add(sale2)
+sale3 = Sale(prise=580, date_sale=datetime(2022, 11, 5), id_stock=3)
+session.add(sale3)
+sale4 = Sale(prise=490, date_sale=datetime(2022, 11, 2), id_stock=4)
+session.add(sale4)
+sale5 = Sale(prise=600, date_sale=datetime(2022, 10, 26), id_stock=5)
+session.add(sale5)
+
+session.commit()
+print(sale1.prise)
+print(sale1.date_sale)
+
 session.close()
 
+def query_input_publisher_name_books(author_name):
+    sql1 = session.query(Publisher, Book, Stock, Shop, Sale).join(Book, Publisher.id == Book.id_publisher).join(Stock, Book.id == Stock.id_book).join(Shop, Stock.id_shop == Shop.id).join(Sale, Stock.id == Sale.id_stock).filter(Publisher.name == author_name).all()
+    for publisher, book, stock, shop, sale in sql1:
+        print(book.title, shop.name, sale.prise, sale.date_sale)
+
+author_name = input("Введите имя автора: ")
+query_input_publisher_name_books(author_name)
--- models.py
+++ models.py
@@ -1,14 +1,17 @@
+import sqlalchemy
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
-
 
 class Publisher(Base):
     __tablename__ = 'publisher'
 
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=40), unique=True)
+
+    # def __str__(self):
+    #     return f'{self.name}: {self.title}'
 
 class Book(Base):
     __tablename__ = 'book'
@@ -17,6 +20,32 @@ class Book(Base):
     title = sq.Column(sq.String(length=40), unique=True)
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
     publisher = relationship(Publisher, backref='book')
+
+class Shop(Base):
+    __tablename__ = 'shop'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True)
+
+class Stock(Base):
+    __tablename__ = 'stock'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    # count = '' нужно понять зачем это нужно и как это сделать
+    id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
+    stock_id_book = relationship(Book, backref='book')
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
+    stock_id_shop = relationship(Shop, backref='shop')
+
+class Sale(Base):
+    __tablename__ = 'sale'
+
+    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
+    prise = sq.Column(sq.Integer, primary_key=True)
+    date_sale = sq.Column(sq.DateTime(), unique=True)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
+    sale_id_stock = relationship(Stock, backref='stock')
+    # count = нужно понять зачем это нужно и как это сделать
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
